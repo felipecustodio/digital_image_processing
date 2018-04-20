98c7e3d2645def7e65d2b829deceef966dfe8a13
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2018-04-20 14:45:36 -0300

    test cases

--- assignment4/tests.sh
+++ assignment4/tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
         rm results.txt
-        for i in `seq 1 9`;
+        for i in `seq 1 3`;
         do
                 printf "\nRunning test $i\n"
                 printf "\nRunning test $i: " >> results.txt
-                python assignment4.py < tests/$i.in >> results.txt
+                # python assignment4.py < tests/$i.in >> results.txt
+                python assignment4.py < tests/$i.in 2>&1 | tee results.txt
         done
